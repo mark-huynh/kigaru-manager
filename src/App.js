a802274876043291b3df5a4ca360de142231f543
Mark <01markhuynh@gmail.com>
2020-03-26 17:05:44 -0700

    Added login ability

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import MenuSection from './MenuSection';
-import './App.css';
-import Amplify, {Auth} from "aws-amplify";
+import React, { useState, useEffect } from "react";
+import MenuSection from "./MenuSection";
+import "./App.css";
+import Amplify, { Auth } from "aws-amplify";
+import { Button, FormGroup, FormControl } from "react-bootstrap";
 
 Amplify.configure({
   region: "us-east-1",
@@ -11,14 +11,76 @@ Amplify.configure({
 });
 
 function App() {
-  // Auth.signIn("kigaru-admin", "ilovesushi12345")
-  //   .then(res => console.log(res))
-  //   .catch(err => console.log(err));
-  return (
-    <MenuSection/>
-    // <div>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    // </div>
+  function validateForm() {
+    return username.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    Auth.signIn(username, password)
+      // .then(data => console.log(data.signInUserSession.idToken.jwtToken))
+      .then(data => setKey(data.signInUserSession.idToken.jwtToken))
+      .then(userHasAuthenticated(true))
+      .catch(e => alert(e.message));
+  }
+
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [authKey, setKey] = useState("");
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+  }
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  function onLoad() {
+    Auth.currentSession()
+      .then(data => console.log(data))
+      .then(userHasAuthenticated(true))
+      .catch(e => {
+        console.log(e);
+        userHasAuthenticated(false);
+      });
+  }
+
+  return (
+    <div>
+      {isAuthenticated ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <div className="Login">
+          <form onSubmit={handleSubmit}>
+            <FormGroup controlId="username">
+              Username
+              <FormControl
+                autoFocus
+                type="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup controlId="password">
+              Password
+              <FormControl
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password"
+              />
+            </FormGroup>
+            <Button block disabled={!validateForm()} type="submit">
+              Login
+            </Button>
+          </form>
+        </div>
+      )}
+      <MenuSection />
+    </div>
   );
 }
 
